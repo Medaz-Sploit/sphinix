d15fed8241218889c32cf274cd40148f5c56ca8c
Sliman Azoukni <slimanaz@Slimans-MacBook-Pro.local>
2024-05-14 18:28:22 +0100

    done

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -19,7 +19,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
+    ("Twiiter", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
     ("You can modify those links in your config file", "#"),
@@ -27,7 +27,7 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
+    ("https://github.com/Medaz-Sploit", "#"),
     ("Another social link", "#"),
 )
 
