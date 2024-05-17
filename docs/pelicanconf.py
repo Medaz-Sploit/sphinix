536a8c0c0c4ce5babe2a4a8146b12dbec225ce79
Kali Live user <kali@localhost.localdomain>
2024-05-17 18:41:04 +0100

    new website

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -18,8 +18,6 @@ DISPLAY_MENU   = True
 DARK_LIGHT_SWITCHING_OFF = True
 
 # provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
-TAGS_URL           = 'tags'
-TAGS_SAVE_AS       = 'tags/index.html'
 AUTHORS_URL        = 'authors'
 AUTHORS_SAVE_AS    = 'authors/index.html'
 CATEGORIES_URL     = 'categories'
@@ -29,15 +27,13 @@ ARCHIVES_SAVE_AS   = 'archives/index.html'
 
 # use those if you want pelican standard pages to appear in your menu
 MENU_INTERNAL_PAGES = (
-    ('Tags', TAGS_URL, TAGS_SAVE_AS),
     ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
     ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
     ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
 )
 # additional menu items
 MENUITEMS = (
-    ('GitHub', 'https://github.com/'),
-    ('Linux Kernel', 'https://www.kernel.org/'),
+    ('GitHub', 'https://github.com/Medaz-Sploit'),
 )
 
 # example pagination pattern
@@ -54,29 +50,5 @@ THEME = "/home/kali/pelican-themes/blue-penguin-dark"
 TIMEZONE = 'Africa/Casablanca'
 
 DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (
-    ("Twiiter", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("https://github.com/Medaz-Sploit", "#"),
-    ("Another social link", "#"),
-)
-
-DEFAULT_PAGINATION = 10
-
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
